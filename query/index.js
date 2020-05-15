cd7e94e3683c8e911703ca6be8fcb3f9fa36146e
Eduardo Pezzi <eduardopezzi@Eduardos-MacBook-Pro.local>
2020-05-15 16:59:11 -0400

    Modarator feature

--- query/index.js
+++ query/index.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
+
+const handlerEvent = (type, data) => {
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  }
+
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+    comment.content = content;
+  }
+};
 
 app.get("/posts", (req, res) => {
   res.send(posts);
@@ -15,22 +42,18 @@ app.get("/posts", (req, res) => {
 app.post("/events", (req, res) => {
   const { type, data } = req.body;
 
-  if (type === "PostCreated") {
-    const { id, title } = data;
+  handlerEvent(type, data);
 
-    posts[id] = { id, title, comments: [] };
-  }
-
-  if (type === "CommentCreated") {
-    const { id, content, postId } = data;
-
-    const post = posts[postId];
-    post.comments.push({ id, content });
-  }
-  console.log(posts);
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("Listening on 4002");
+  const res = await axios.get("http://localhost:4005/events");
+
+  for (let event of res.data) {
+    console.log("processing event: ", event.type);
+
+    handlerEvent(event.type, event.data);
+  }
 });
